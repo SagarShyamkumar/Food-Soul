bd1ad4c462b82a87f59c8430f0edac3fbfbf7b75
Sagar Shyamkumar <Sagar07shyam@gmail.com>
2025-03-28 06:20:28 +0530

    Completed the Intropage with a button to redirect to Login page added as commented out
    Started work on AboutUs page and the 1st main page named Canon1.

--- app/lib/pages/Intropage.dart
+++ app/lib/pages/Intropage.dart
@@ -1,48 +1,70 @@
 import 'package:flutter/material.dart';
-import 'package:lib/pages/Login.dart';
+import 'Login.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-Class Intropage extends StatelessWidget {
+class Intropage extends StatelessWidget {
   const Intropage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.yellow[200],
-        body: Center{
-            child: Column{
-                children:[
-                    padding: EdgeInsets.all(horizontal:20,0),
-                    Child: Image.asset(
-                        'lib/Images/FoodSoul Logo.png', 
-                        height: 250
-                    ),
-                    const SizedBox(height: 48),
-                    const Text(
-                        'Welcome to FoodSoul',
-                        style: TextStyle(
-                            Colors.green[300]
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold
-                        )
-                    )
-                    textAlign:Center,
-                    const Text(
-                        'Where Souls Meet Hearts',
-                        style: TextStyle(
-                            Colors.lightGreen[300]
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold
-                        )
-                    ),
-                    const SizedBox(height: 48),
-                    Container
-                ]
-            }
-        }
+      backgroundColor: Colors.amber[200],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Image.asset(
+                'lib/Images/FoodSoul Logo.png', 
+                height: 250,
+              ),
+            ),
+            const SizedBox(height: 48),
+            Text(
+              'Welcome to FoodSoul',
+              style: TextStyle(
+                color: Colors.green[800],
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              'Where Souls Meet Hearts',
+              style: TextStyle(
+                color: Colors.lightGreen[500],
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 48),
+            GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Login(),
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange[300],
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                padding: const EdgeInsets.all(20),
+                child: const Text(
+                  'Contribute', 
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
